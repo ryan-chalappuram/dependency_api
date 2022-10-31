437357187cddfb417aa95ac18d3eb31e1198607f
Ryan <ryanchalappuram@gmail.com>
2022-11-01 00:04:11 +0530

    Base

---
+++ src/test/java/com/dependency/demo/DependencyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dependency.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DependencyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
