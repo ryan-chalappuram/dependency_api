a1f54b61659932a2ff19436340782a8369101af9
Ryan <ryanchalappuram@gmail.com>
2022-11-07 23:32:28 +0530

    xmlreadpart

--- src/main/java/com/dependency/demo/DependencyService.java
+++ src/main/java/com/dependency/demo/DependencyService.java
@@ -1,21 +1,28 @@
-package com.dependency.demo;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-
-@RestController
-@RequestMapping(path = "api")
-public class DependencyService {
-    private final DependencyController dependencyController;
-
-@Autowired
-public DependencyService(DependencyController dependencyController) {
-        this.dependencyController = dependencyController;
-    }
+//package com.dependency.demo;
+//
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.web.bind.annotation.GetMapping;
+//import org.springframework.web.bind.annotation.RequestMapping;
+//import org.springframework.web.bind.annotation.RestController;
+//
+//import javax.xml.bind.JAXBException;
+//import java.io.IOException;
+//
+//@RestController
+//
+//public class DependencyService {
+//    private final DependencyController dependencyController;
+//
+//@Autowired
+//public DependencyService(DependencyController dependencyController) {
+//        this.dependencyController = dependencyController;
+//    }
+//    @RequestMapping("/")
+//    public xmlclass getlang() throws JAXBException, IOException {
+//    return dependencyController.unmarshall();
+//    }
 //@GetMapping
 //    public String getLang () {
 //        return dependencyController.langgetter();
 //    }
-}
+//}
